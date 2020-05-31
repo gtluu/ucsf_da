759a3664bd70f6e707a70123a6d155987c7a23b9
gtluu <gtluu912@gmail.com>
2020-05-31 16:50:51 -0500

    add new pages; registration, login, user managment functions

--- tracking_tool/__init__.py
+++ tracking_tool/__init__.py
@@ -1,14 +1,21 @@
 import flask
-from  flaskext.mysql import MySQL
+from flaskext.mysql import MySQL
+from tracking_tool import *
+from datetime import timedelta
+import random
+import string
+import hashlib
 
 
 app = flask.Flask(__name__)
+app.secret_key = 'fuzzybuddy'
+
 mysql = MySQL()
+mysql.init_app(app)
 
 app.config['MYSQL_DATABASE_USER'] = 'root'
-#app.config['MYSQL_DATABASE_PASSWORD'] = input('MySQL Password: ')
-app.config['MYSQL_DATABASE_PASSWORD'] = '00krusty*'
-app.config['MYSQL_DATABSE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PASSWORD'] = input('MySQL Password: ')
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_DB'] = 'ucsfda'
 
 
--- tracking_tool/views.py
+++ tracking_tool/views.py
@@ -1,5 +1,37 @@
 import flask
-from tracking_tool import app, MySQL
+from tracking_tool import *
+from flaskext.mysql import MySQL
+import flask_login
+
+
+class User():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.id = None
+        self.auth = None
+
+    def authenticate(self):
+        cur = mysql.connect().cursor()
+        cur.execute("SELECT username, password, salt, id, authorization FROM users WHERE username = '" +
+                    self.username + "'")
+        username, password, salt, id, authorization = cur.fetchone()
+        cur.close()
+        self.password = hashlib.sha512(self.password.encode('utf-8') + salt.encode('utf-8')).hexdigest()
+        if self.username == username and self.password == password:
+            self.id = id
+            self.auth = int(authorization)
+            return True
+
+
+def check_session(page):
+    try:
+        if flask.session['logged_in']:
+            return flask.render_template(page)
+        else:
+            return flask.render_template('index.html')
+    except KeyError:
+        return flask.render_template('index.html')
 
 
 @app.route('/')
@@ -15,9 +47,13 @@ def login_page():
 @app.route('/loginnewuser', methods=['POST'])
 def login():
     form = flask.request.form.to_dict()
-    # replace with checking user id and password in sql database
-    # password hashing/salting
-    if form['username'] == '' and form['password'] == '':
+
+    flask.session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+    user = User(form['username'], form['password'])
+    if user.authenticate():
+        flask.session['access'] = user.auth
+        flask.session['logged_in'] = True
         return flask.render_template('advisor_home.html')
     else:
         return flask.render_template('index.html')
@@ -28,35 +64,118 @@ def registration_page():
     return flask.render_template('register.html')
 
 
-@app.route('/registrationnewuser', methods=['POST'])
+@app.route('/registernewuser', methods=['POST'])
 def register():
     form = flask.request.form.to_dict()
-    # replace with checking user id and password in sql database
-    # password hashing/salting
-    # create new user in db
-    pass
+    if form['password'] != form['password_re']:
+        return flask.render_template('registration_password_mismatch.html')
+
+    query = 'INSERT INTO users (id, authorization, ucsf_da_id, username, salt, password) VALUES '
+    con = mysql.connect()
+    cur = con.cursor()
+    # generate system user id
+    cur.execute('SELECT id FROM users')
+    id_list = cur.fetchall()
+    id_list = [i[0] for i in id_list]
+    while True:
+        id = random.randint(100000, 999999)
+        if id not in id_list:
+            break
+
+    # set authorization level
+    # 0 == su, 1 == admin, 2 == advisor, 3 == student, 4 == parent, 5 == inactive
+    cur.execute('SELECT id FROM admins')
+    admin_id_list = cur.fetchall()
+    admin_id_list = [i[0] for i in admin_id_list]
+    cur.execute('SELECT id FROM admins')
+    advisor_id_list = cur.fetchall()
+    advisor_id_list = [i[0] for i in advisor_id_list]
+    cur.execute('SELECT id FROM admins')
+    student_id_list = cur.fetchall()
+    student_id_list = [i[0] for i in student_id_list]
+    cur.execute('SELECT id FROM admins')
+    parent_id_list = cur.fetchall()
+    parent_id_list = [i[0] for i in parent_id_list]
+    is_admin = form['id'] in admin_id_list
+    is_advisor = form['id'] in advisor_id_list
+    is_student = form['id'] in student_id_list
+    is_parent = form['id'] in parent_id_list
+    if is_admin and not is_advisor and not is_student and not is_parent:
+        auth = 1
+    elif is_advisor and not is_admin and not is_student and not is_parent:
+        auth = 2
+    elif is_student and not is_admin and not is_advisor and not is_parent:
+        auth = 3
+    elif is_parent and not is_admin and not is_advisor and not is_student:
+        auth = 4
+    else:
+        auth = 5
+
+    # get salt
+    salt = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(128))
+    # encode password
+    password = hashlib.sha512(form['password'].encode('utf-8') + salt.encode('utf-8')).hexdigest()
+
+    # add to users database
+    values = (str(id), str(auth), str(form['id']), "'" + form['username'] + "'", "'" + salt + "'", "'" + password + "'")
+    query += '(' + ', '.join(values) + ');'
+    cur.execute(query)
+    con.commit()
+    cur.close()
+    return flask.render_template('registration_complete.html')
 
 
-@app.route('/advisor_home')
-def advisor_home():
-    return flask.render_template('advisor_home.html')
+@app.route('/home')
+def home():
+    if flask.session['access'] == 0:
+        return check_session('su_home.html')
+    elif flask.session['access'] == 1:
+        return check_session('admin_home.html')
+    elif flask.session['access'] == 2:
+        return check_session('advisor_home.html')
+    elif flask.session['access'] == 3:
+        return check_session('student_home.html')
+    elif flask.session['access'] == 4:
+        return check_session('parent_home.html')
+    else:
+        # no access to page
+        return flask.render_template('restricted_message.html')
 
 
 @app.route('/forms')
 def forms():
-    return flask.render_template('forms.html')
+    if flask.session['access'] <= 2:
+        return check_session('forms.html')
+    else:
+        return flask.render_template('restricted_message.html')
 
 
 @app.route('/new_student_form')
 def new_student_form():
-    return flask.render_template('new_student_form.html')
+    if flask.session['access'] <= 2:
+        return check_session('new_student_form.html')
+    else:
+        return flask.render_template('restricted_message.html')
 
 
 @app.route('/student_status_change')
 def student_status_change():
-    return flask.render_template('student_status_change.html')
+    if flask.session['access'] <= 2:
+        return check_session('student_status_change.html')
+    else:
+        return flask.render_template('restricted_message.html')
 
 
 @app.route('/students')
 def students():
-    return flask.render_template('students.html')
+    if flask.session['access'] <= 2:
+        return check_session('students.html')
+    else:
+        return flask.render_template('restricted_message.html')
+
+
+@app.route('/logout')
+def logout():
+    flask.session['logged_in'] = False
+    flask.session.pop('username', None)
+    return flask.render_template('index.html')
