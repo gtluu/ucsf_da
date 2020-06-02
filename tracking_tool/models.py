b66ef6f4e14c848edbb47df44d658979c9f5ace7
sarahwoldemariam <sarah.woldemariam@gmail.com>
2020-06-01 22:48:53 -0700

    Added login functionality. User table created using SQLAlchemy.

--- tracking_tool/models.py
+++ tracking_tool/models.py
@@ -1,14 +1,21 @@
 from tracking_tool import db, login_manager
-from datetime import datetime
 from flask_login import UserMixin
+
+
+@login_manager.user_loader
+# check if User authenticated, active, annonymous, get ID
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     # columns for table
-    username = username = db.Column(db.String(45), unique=True, nullable = False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    username = db.Column(db.String(45), unique=True, nullable = False)
     password = db.Column(db.String(60), nullable=False)
-    id = db.Column(db.Integer, primary_key = True) # primay_key indicates unique identifier
+    authorization = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)  # primay_key indicates unique identifier
 
     def __repr__(self): # how object is printed
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}')"
 
