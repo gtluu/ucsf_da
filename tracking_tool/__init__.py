50d50e3c921185ad2097d3df889afd042f609987
sarahwoldemariam <sarah.woldemariam@gmail.com>
2020-06-01 22:53:04 -0700

    Added SQLAlchemy, Bcrpyt, and login functionality.

--- tracking_tool/__init__.py
+++ tracking_tool/__init__.py
@@ -1,12 +1,24 @@
-import flask
+from flask import Flask
 from flaskext.mysql import MySQL
-from tracking_tool import *
 from datetime import timedelta
 import random
 import string
 import hashlib
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'GjHPucpWicgF9jVmKOQ1s31xunFbjuY3ir3O9yMhoYpz2K6N2ktFvQ' # this is for security; on terminal, can import secrets, then run secrets.token_hex(16)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ucsf_da.db'
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+
+'''
 app = flask.Flask(__name__)
 app.secret_key = 'fuzzybuddy'
 
@@ -18,5 +30,5 @@ app.config['MYSQL_DATABASE_PASSWORD'] = input('MySQL Password: ')
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_DB'] = 'ucsfda'
 
-
+'''
 import tracking_tool.views
