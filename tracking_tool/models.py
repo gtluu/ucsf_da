ef59544b4043d13814bc3d1b1635e31a4b00d793
sarahwoldemariam <sarah.woldemariam@gmail.com>
2020-06-04 14:14:28 -0700

    created ucsfda tables using SQLAlchemy

--- tracking_tool/models.py
+++ tracking_tool/models.py
@@ -11,11 +11,63 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     # columns for table
 
+    id = db.Column(db.Integer, primary_key=True, unique=True)  # primary_key indicates unique identifier
+    authorization = db.Column(db.Integer, nullable=False)
+    ucsf_da_id = db.Column(db.Integer, unique=True, nullable=False)
     username = db.Column(db.String(45), unique=True, nullable = False)
     password = db.Column(db.String(60), nullable=False)
-    authorization = db.Column(db.Integer, nullable=False)
-    id = db.Column(db.Integer, primary_key=True)  # primay_key indicates unique identifier
 
     def __repr__(self): # how object is printed
         return f"User('{self.username}')"
+
+class Admins(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    middle_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    cell_phone = db.Column(db.String(10))
+    work_phone = db.Column(db.String(10), nullable=False)
+    home_phone = db.Column(db.String(10), nullable=False)
+
+class Advisors(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    middle_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    cell_phone = db.Column(db.String(10))
+    work_phone = db.Column(db.String(10), nullable=False)
+    home_phone = db.Column(db.String(10), nullable=False)
+    school = db.Column(db.String(60), nullable=False)
+
+class Parents(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    middle_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    cell_phone = db.Column(db.String(10))
+    work_phone = db.Column(db.String(10))
+    home_phone = db.Column(db.String(10), nullable=False)
+    student_id = db.Column(db.Integer, nullable=False)
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(60), nullable=False)
+    middle_name = db.Column(db.String(60))
+    last_name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    cell_phone = db.Column(db.String(10))
+    work_phone = db.Column(db.String(10))
+    home_phone = db.Column(db.String(10), nullable=False)
+    school = db.Column(db.String(60), nullable=False)
+    grade = db.Column(db.String(16), nullable=False)
+    expected_grad = db.Column(db.Integer, nullable=False)
+    gpa = db.Column(db.String(60), nullable=False)
+    program_status = db.Column(db.String(16), nullable=False)
+    fmp_id = db.Column(db.Integer, unique=True, nullable=False)
+    parent_1_id = db.Column(db.Integer, nullable=False)
+    parent_2_id = db.Column(db.Integer, nullable=False)
+    advisor_id = db.Column(db.Integer, nullable=False)
 
